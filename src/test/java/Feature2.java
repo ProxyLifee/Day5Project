dbb9d43381f663b8f68af18b5e44b2240bd4720f
EU3 Batch <av.serdaralp@gmail.com>
2020-10-15 04:08:29 +0100

    Awesome feature 2 added more changes to line 3

--- src/test/java/Feature2.java
+++ src/test/java/Feature2.java
@@ -4,6 +4,8 @@ public class Feature2 {
         System.out.println("Working on Feature2 more");
         System.out.println("Working on Feature2 again");
 
+        System.out.println("Adding some more changes to line 3, virtually");
+
 
     }
 }
