3ae67ea4feda71924538eb7e168c5172bfbb558d
ProxyLifee <70109365+ProxyLifee@users.noreply.github.com>
2020-10-15 06:22:48 +0100

    Added changes to hello from GitHub

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Sill really awesome hello");
+        System.out.println("Sill really FROM GitHub MADE SOME CHANGES awesome hello");
     }
 }
