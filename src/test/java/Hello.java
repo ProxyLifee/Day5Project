8a99b3a47006c09fa7338cff2b8bbef5bf8bb50a
EU3 Batch <av.serdaralp@gmail.com>
2020-10-15 06:16:04 +0100

    updated we did say hello

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Sill awsome hello");
+        System.out.println("Sill really awesome hello");
     }
 }
