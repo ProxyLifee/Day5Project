5bc4a895b327f33808bc47376bf820d0d08f830a
EU3 Batch <av.serdaralp@gmail.com>
2020-10-15 06:25:26 +0100

    added comment on same line

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Sill really awesome hello");
+        System.out.println("Sill really awesome hello"); // added comment
+
+
     }
 }
